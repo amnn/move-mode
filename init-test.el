c405ed8cc3357f438b4e7c9f5fd8459f2b9dad59
Ashok Menon <ashok@mystenlabs.com>
2022-11-27 17:59:43 +0000

    [EASY][BUGFIX] Fix path to fixture in init-test.el

--- init-test.el
+++ init-test.el
@@ -9,4 +9,4 @@
   (add-to-list 'load-path (file-name-directory load-file-name))
   (require 'move-mode)
 
-  (find-file (concat repo-root "test/sources/example.move")))
+  (find-file (concat repo-root "test/highlight/sources/example.move")))
